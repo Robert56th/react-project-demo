7fbdbf1e354e1e2fcfd00bb52b0a99874ff286dd
CO, JUSTIN MIGUEL ALMAZAN <83590278+1n1ts1x@users.noreply.github.com>
2022-03-29 16:58:33 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
-import GameCatalog from "./GameCat";
 import Navbar from "./Navbar";
 import Home from "./Home";
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import SignIn from "./SignIn";
+import GameCatalog from "./GameCat";
+import { useState } from "react"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [games, setGames] = useState([
+    {
+        id: 1,
+        text: 'game1',
+        status: true
+    },
+    {
+        id: 2,
+        text: 'game2',
+        status: false
+    },    
+  ])
+
+  const addGame = (game) => {
+    const id = () => games.length + 1
+    const newGame = {id, ...game}
+    setGames([...games, newGame])
+  }
+
+  const deleteGame = (id) => {
+    setGames(games.filter((game) => game.id !== id))
+  }
+
   return (
     <Router>
       <div className="App">
         <Navbar/>
         <Routes>
-          <Route exact path="/" element={<Home/>}/>
-          <Route exact path="/create" element={<GameCatalog/>}/>
+          <Route exact path="/home" element={<Home/>}/>
+
+          <Route exact path="/create" element={games.length > 0 ?
+          <GameCatalog games={games} onAdd={addGame} onDelete={deleteGame}/> : 
+          ('No games to show')}/>
+
+          <Route exact path="/signin" element={<SignIn/>}/>
         </Routes>
       </div>
     </Router>
