b09b6a52c4b4b4327584e4579545b9301aeae6da
Adrian Robert Doroteo <adrian56th@gmail.com>
2022-03-30 18:14:08 +0800

    Revert "Update App.js"

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,10 +2,8 @@ const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>Game Catalogue</h1>
-
             <div className="links">
-                <a href ="/home"> Home</a>
-                <a href ="/signin"> Sign In</a>
+                <a href ="/"> Home</a>
                 <a href ="/create"> Create</a>
             </div>
         </nav>
