00f6896e0a6db5cbaa26c86e949e3774a06642ca
migsunit21 <deveraturda.miguel@gmail.com>
2022-04-09 19:51:21 +0800

    feat: add homepage grid layout

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
-    return ( 
-        <h1>Homepage mga tropa</h1>
-     );
-}
- 
+  return (
+    <div className="homepage-parent">
+      <div className="homepage">
+        <div className="welcome">welcome</div>
+
+        <div className="content1">content1</div>
+
+        <div className="content2">content2</div>
+
+        <div className="content3">content3</div>
+      </div>
+    </div>
+  );
+};
+
 export default Home;
