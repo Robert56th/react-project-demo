747700c1d38f9edbf1d54ae71596f8f80e112d01
Adrian Robert Doroteo <adrian56th@gmail.com>
2022-05-01 21:02:11 +0800

    Fixed errors in game prog to deploy to netlify

--- src/GameProg.js
+++ src/GameProg.js
@@ -5,19 +5,19 @@ const GameProg = ({ progress, toggleProgress }) => {
                 <h3>Progress</h3>
                 <div>
                     <label>
-                        <input type="radio" value="Unfinished" checked={progress == 'Unfinished'} onChange={toggleProgress} />
+                        <input type="radio" value="Unfinished" checked={progress === 'Unfinished'} onChange={toggleProgress} />
                         <span>Unfinished</span>
                     </label>
                     <label>
-                        <input type="radio" value="Beaten" checked={progress == 'Beaten'} onChange={toggleProgress} />
+                        <input type="radio" value="Beaten" checked={progress === 'Beaten'} onChange={toggleProgress} />
                         <span>Beaten</span>
                     </label>
                     <label>
-                        <input type="radio" value="Completed" checked={progress == 'Completed'} onChange={toggleProgress} />
+                        <input type="radio" value="Completed" checked={progress === 'Completed'} onChange={toggleProgress} />
                         <span>Completed</span>
                     </label>
                     <label>
-                        <input type="radio" value="Unplayed" checked={progress == 'Unplayed'} onChange={toggleProgress} />
+                        <input type="radio" value="Unplayed" checked={progress === 'Unplayed'} onChange={toggleProgress} />
                         <span>Unplayed</span>
                     </label>
                 </div>
